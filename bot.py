0ecc92780106ac72fdc4c1c63e74454262d43f7a
Taptaplit <74630038+Taptaplit@users.noreply.github.com>
2021-04-07 20:46:02 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,18 +1,87 @@
-import smtplib
+import bcrypt
 
-server = smtplib.SMTP('smtp.gmail.com', 587)
-server.starttls()
+text = input('Text: ')
 
-github_url = str("https://github.com/Taptaplit/email-bot")
 
-name = str(input("\nWhat is your name: "))
-from_email = str(input("\nYour Email: "))
-send_to = str(input("\nEmail of who you want to send it to: "))
-subject = str(input("\nWhat do you want the subject to be? "))
-msg = str(input(f"\nMessage you want to send to {send_to}: "))
 
-server.login('EMAIL', 'EMAIL-PASS')
-server.sendmail(f'{from_email}',
-                f'{send_to}',
-                f'Subject: {subject}\n\nFrom: {name} | {name}`s Email: {from_email}\nMessage: {msg}\n\n Sent with: {github_url}\n **Note {name} sent this from a bot, and they will not be reciving any replys from this conversation, so email them back on their own email!'
-                )
+class Hash_Encryption():
+    def __init__(self, salt):
+        self.salt = salt
+        self.abc = ['n', 'Z', 'c', ')', 'o', 'f', 'A', '*', '{', 'D', 'B', 'w', 'u', 'K', 'W', 'q', 'T', ']', 'd', 'e', '(', 'V', '/', '[', 'a', 'N', 'J', '^', 'E', 'm', 'y', 'F', 'S', ';', 'r', 'U', 'i', 'j', 'g', '0', '.', 'P', 'H', 'z', '<', '9', '`', 'x', '3', '5', 'C', '|', 'R', '+', '6', 'v', '@', '}', '&', '-', '~', 'b', 't', '4', '1', ':', 'k', 'L', '$', '>', '8', '7', '%', 'M', 'Y', 'X', 'I', 'O', '2', 'Q', 'h', 's', '?', 'l', 'p', 'G', "'"]
+        self.charLength = int(len(self.abc) - 1)
+
+    def check_letter(self, iL):
+        for i, letter in enumerate(self.abc):
+            if letter == iL:
+                return i
+
+    def encode(self, user_i):  
+        user_i = user_i[::-1]        
+        encoded = '' 
+        for char in user_i:
+            if char != ' ':
+                index = self.check_letter(char)
+                if int(index + 5) > self.charLength:
+                    letter = self.abc[(int(index + 5) - self.charLength) - 1]
+                else: 
+                    letter = self.abc[int(index + 5)]
+            else:
+                letter = '_'
+            encoded += str(letter)
+        return encoded
+            
+            
+    def decode(self, user_i):
+        user_i = user_i[::-1]        
+        decoded = '' 
+        for char in user_i:
+            if char != '_':
+                index = self.check_letter(char)
+                if int(index - 5) < 0:
+                    letter = self.abc[(int(index - 5) + self.charLength) + 1]
+                else: 
+                    letter = self.abc[int(index - 5)]
+            else:
+                letter = ' '
+            decoded += str(letter)
+        return decoded
+    
+    
+    
+    def hashText(self, text):
+        bcryptText = bytes(text, 'utf-8')
+        hashed = bcrypt.hashpw(bcryptText, bcrypt.gensalt())
+        
+        return hashed
+
+    def confirmHasedText(self,hash, text):
+        bcryptText = bytes(text, 'utf-8')
+        if bcrypt.checkpw(bcryptText, hash):
+            return True
+        return False
+
+    def hack_and_encrypt(self,input):   
+        hash = self.hashText(input)
+        encoded = self.encode(hash.decode())
+        
+        return hash, encoded
+
+
+    def crack(self, guess, encoded, hash):
+        hash = self.decode(encoded)
+        bcryptHash = bytes(hash, 'utf-8')
+        check = self.confirmHasedText(bcryptHash, guess)
+        return check
+
+
+lockText = Hash_Encryption(19)
+hash, encoded = lockText.hack_and_encrypt(text)
+
+
+guess = 'hello'
+output = lockText.crack(guess, encoded, hash)
+print(output)
+
+
+    
+    
